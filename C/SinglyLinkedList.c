1d1bac74799665ebbb40366bb1f22209447df2fd
unknown <gonzalezricardo315@yahoo.com>
2022-09-28 01:16:43 -0600

    adding what i have done for the night still need to work on this a lot.

--- C/SinglyLinkedList.c
+++ C/SinglyLinkedList.c
@@ -1,5 +1,43 @@
 #include <stdio.h>
 
+struct Node
+{
+    int data;
+    struct node *node;
+};
+    
+class SinglyLinkedList
+{
+    public: node *head;
+    public: node *tail;
+    public: linked_list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void addNode(int n)
+    {
+        node *temp = new nodel
+        temp->data = n;
+        temp->next = NULL;
+
+        if(head == null)
+        {
+            head = temp;
+            tail = temp;
+        }
+        else 
+        {
+            tail->next =temp;
+            tail = tail->temp;
+        }
+    }
+};
+    
+
 int main() {
+    linked_list sList;
+    a.
     return 0;
 }
